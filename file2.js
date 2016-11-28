aa48de6dbefbb346353a28e29922fdb1c93a1620
Preston Buckley <prestonbuckley@gmail.com>
2016-11-28 15:10:31 -0700

    the last of the commits for this CL request

--- file2.js
+++ file2.js
@@ -5,3 +5,7 @@ var journal = [
 var journal2 = [
   "This is not easy stuff, but I am willing to learn to benefit"
 ];
+
+var journal3 = [
+  "Definitely going to be a difficult 3 months for me"
+];
